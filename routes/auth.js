7bc4baf2ba3d5d1faf492e072aa425aa2d101ff8
Jasper Van der Voo <jasper.voo@gmail.com>
2019-07-06 18:16:15 +1200

    Initial Commit

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const axios = require("axios");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+router.post("/products", cors(), async (req, response) => {
+	dotenv.config();
+	try {
+		const instance = axios.create({
+			auth: {
+				username: process.env.WOO_CK,
+				password: process.env.WOO_CS
+			}
+		});
+		const result = await instance.get(`https://react.alphabean.co.nz/wp-json/wc/v2/products/6271/variations`);
+		response.send(result.data[0].price);
+	} catch (err) {
+		response.send("Error: " + err);
+	}
+});
+
+//router.post('/login')
+
+module.exports = router;
