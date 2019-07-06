ad519c1231de046338fe3cf251dde18c649f59a1
Jasper Van der Voo <jasper.voo@gmail.com>
2019-07-06 21:16:19 +1200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,21 +2,22 @@ const router = require("express").Router();
 const axios = require("axios");
 const dotenv = require("dotenv");
 
-router.get("/products", async function(req, response) {
+router.get("/products", function(req, response) {
 	dotenv.config();
-	try {
-		const instance = axios.create({
-			auth: {
-				username: process.env.WOO_CK,
-				password: process.env.WOO_CS
-			}
-		});
-		const result = await instance.get(`https://react.alphabean.co.nz/wp-json/wc/v2/products/6271/variations`);
+	// try {
+	// 	const instance = axios.create({
+	// 		auth: {
+	// 			username: process.env.WOO_CK,
+	// 			password: process.env.WOO_CS
+	// 		}
+	// 	});
+	// 	const result = await instance.get(`https://react.alphabean.co.nz/wp-json/wc/v2/products/6271/variations`);
 
-		response.send(result.data[0].price);
-	} catch (err) {
-		response.send("Error: " + err);
-	}
+	// response.send(result.data[0].price);
+	response.send("50");
+	// } catch (err) {
+	// 	response.send("Error: " + err);
+	// }
 });
 
 module.exports = router;
