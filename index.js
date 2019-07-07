3b480f1adb92eeb957d5f03f77a72684c4db7e3d
Jasper Van der Voo <jasper.voo@gmail.com>
2019-07-07 15:03:59 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
 
 //Import middleware
 const authRoute = require("./routes/auth");
@@ -9,6 +10,24 @@ app.use("/api/wc/", authRoute);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/a/b/c", (req, res) => res.send("Hello World! (/a/b/c)"));
+app.get("/api/wc/products", (req, res) => {
+	dotenv.config();
+	try {
+		const instance = axios.create({
+			auth: {
+				username: process.env.WOO_CK,
+				password: process.env.WOO_CS
+			}
+		});
+		// const result = await instance.get(`https://react.alphabean.co.nz/wp-json/wc/v2/products/6271/variations`);
+
+		instance.get(`https://react.alphabean.co.nz/wp-json/wc/v2/products/6271/variations`).then(res => {
+			response.send(res.data);
+		});
+		// response.send(result.data[0].price);
+	} catch (err) {
+		response.send("Error: " + err);
+	}
+});
 
 app.listen(8081, () => console.log("Server up and running on port 8081"));
